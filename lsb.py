77464a84d765e729aa82f53e30e1b17ad748d3e6
noursetiha <153625604+noursetiha@users.noreply.github.com>
2023-12-25 01:06:01 +0200

    create lsb decode function

--- lsb.py
+++ lsb.py
@@ -79,3 +79,26 @@ def encode2(im_path,secret):
     new_img_name = im_path[:im_path.find(".")]+"steg"+im_path[im_path.find("."):]
     newimg.save(new_img_name)
     newimg.show()
+def decode2(path):
+    image = Image.open(path, 'r')
+
+    secret = ''
+    imgdata = iter(image.getdata())
+
+    while (True):
+        pixels = [value for value in imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3] +
+                                imgdata.__next__()[:3]]
+
+        # string of binary data
+        binstr = ''
+
+        for i in pixels[:8]:
+            if (i % 2 == 0):
+                binstr += '0'
+            else:
+                binstr += '1'
+
+        secret += chr(int(binstr, 2))
+        if (pixels[-1] % 2 != 0):
+            return (secret)
