b21e4bf5741b38cbffd229ae375a9e8817ff796d
A7med-Elbagoury <me3289762@gmail.com>
2023-12-25 02:06:27 +0200

    create the struction of page

--- page2.py
+++ page2.py
@@ -50,3 +50,65 @@ def Dispage2():
 
             else:  # --->decod+LSB
                 decode2(file_path)
+
+    global pro
+    pro = Tk()
+    pro.title('Security Tool')
+    pro.geometry("700x500+350+90")
+    pro.resizable(False, False)
+    pro.config(background='#3E3646')
+    pro.iconbitmap('hider.ico')
+    style = ttk.Style()
+    style.configure("TRadiobutton", background="#A6A4A7")
+
+    But1 = Button(pro, text="Home", foreground='white', background="#A6A4A7", font=20, command=action)
+    But1.place(x=10, y=10, width=150, height=30)
+
+    But2 = Button(pro, text="About", foreground='white', background="#A6A4A7", font=20)
+    But2.place(x=170, y=10, width=150, height=30)
+
+    lb1 = Label(pro, text="Video steganography ", foreground='white', bg="#3E3646", font=("Arial", 30, "bold"))
+    lb1.place(x=165, y=80)
+
+    lb2 = Label(pro, text="enter secret message :", foreground='white', bg="#3E3646", font=30)
+    lb2.place(x=20, y=150)
+
+    ent = Entry(pro, foreground='black', bg="#A6A4A7")
+    ent.place(x=250, y=155, width=180, height=25)
+
+    lb3 = Label(pro, text="choose image file :", foreground='white', bg="#3E3646", font=30)
+    lb3.place(x=20, y=195)
+
+    But3 = Button(pro, text="Browse", command=browse_file, foreground='white', bg="#A6A4A7")
+    But3.place(x=250, y=200, width=180, height=25)
+
+    lb4 = Label(pro, text="choose option :", foreground='white', bg="#3E3646", font=30)
+    lb4.place(x=22, y=250)
+
+    var1 = IntVar()
+    var2 = StringVar()
+    rad = ttk.Radiobutton(pro, text="Encode", value=1, command=choose1, variable=var1)
+    rad.place(x=200, y=255)
+
+    rad2 = ttk.Radiobutton(pro, text="decode", value=2, command=choose2, variable=var1)
+    rad2.place(x=415, y=255, )
+
+    lb5 = Label(pro, text="choose technique :", foreground='white', bg="#3E3646", font=30)
+    lb5.place(x=22, y=290)
+
+    rad3 = ttk.Radiobutton(pro, text="Append", value="a", command=choosetec1, variable=var2)
+    rad3.place(x=200, y=295)
+
+    rad4 = ttk.Radiobutton(pro, text="LSB(more secure)", value="b", command=choosetec2, variable=var2)
+    rad4.place(x=415, y=295, )
+
+    But4 = Button(pro, text="Apply", foreground="white", background="#A6A4A7", font=20, command=getpath)
+    But4.place(x=250, y=350, width=180, height=50)
+
+
+
+
+
+# def gotopage1():
+#     page2.destroy()  # Close the page2 window
+#     root.deiconify()  # Show the root window
