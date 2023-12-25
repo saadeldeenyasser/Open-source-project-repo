f0d3f2658a70089446e2843483c5bc1575831f1d
A7med-Elbagoury <me3289762@gmail.com>
2023-12-25 02:02:31 +0200

    create action fanction

---
+++ page2.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from append import encode, decode
+from tkinter import filedialog
+from tkinter import ttk
+from lsb import encode2, decode2
+# root.iconify()  # Hide the root window
+
+def action():
+    pro.destroy()
+    from tist import page1
+    page1()
